bc3a8ee9d677e39b9b342f2eddde0d9fae5126a7
zhz55 <kasim@126.com>
2017-07-12 20:33:26 +0800

    2 ways to get kudu data

--- src/main/scala/GetKuduData.scala
+++ src/main/scala/GetKuduData.scala
@@ -8,13 +8,16 @@ object GetKuduData {
   def main(args: Array[String]): Unit = {
     val conf = new SparkConf().setAppName("GetKuduData").setMaster("yarn")
     val sparkSession = SparkSession.builder().config(conf).getOrCreate()
-    val df = sparkSession.read.options(Map("kudu.master" -> "nn01",
-      "kudu.table" -> "impala::position.CTTIC_VehiclePosition_201707_2_test")).kudu
-    //val kuduContext = new KuduContext("nn01")
-    //kuduContext.kuduRDD(sparkSession.sparkContext, "impala::position.CTTIC_VehiclePosition_201707_err_test")
-    df.createOrReplaceTempView("tmp")
+    //val df = sparkSession.read.options(Map("kudu.master" -> "nn01",
+    //  "kudu.table" -> "impala::position.CTTIC_VehiclePosition_201707_2_test")).kudu
+    val kuduContext = new KuduContext("nn01")
+    kuduContext.kuduRDD(sparkSession.sparkContext, "impala::position.CTTIC_VehiclePosition_201707_err_test", Seq("vehicleno", "positiontime")).
+      foreach {
+        record => println(record.mkString(","))
+      }
+    //df.createOrReplaceTempView("tmp")
 
-    df.sqlContext.sql("select * from tmp where positiontime > 1499860032").foreach(println(_))
+    //df.sqlContext.sql("select * from tmp where positiontime > 1499860032").foreach(println(_))
 
   }
 }
